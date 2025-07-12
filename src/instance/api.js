32c1a0a76215cb71f253bd3c68371251e24b07fa
JHONATA LOPES <jhone454@gmail.com>
2025-07-12 16:49:11 -0300

    Update api.js
    ajuste do local host

--- src/instance/api.js
+++ src/instance/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:6780'
+    baseURL: 'https://deploy-api-anime-v2.onrender.com'
 })
 
 instance.interceptors.request.use((config) => {
